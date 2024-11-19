367e903b5013dd8dfe901caa1683a1114b233623
Emily Kemp <emilyandalicekemp@gmail.com>
2024-11-20 07:59:11 +1300

    starting message app with socket.io

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+});
+
+server.listen(5174, () => console.log("Server is running."));
