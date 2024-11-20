f01879a81c5cb163179bc8c3f5e2a53165afd7ba
Emily Kemp <emilyandalicekemp@gmail.com>
2024-11-21 08:29:21 +1300

    added inputs for room and message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messagesReceived, setMessagesReceived] = useState([]);
+  const [messagesSent, setMessagesSent] = useState([]);
+  const [room, setRoom] = useState("");
 
   return (
-    <>
+    <div className="master">
+      <div className="container">
+        <input type="text" className="input" placeholder="Room..." />
+        <button className="button">Join</button>
+      </div>
+      <div className="container">
+        <input type="text" className="input" placeholder="Message..." />
+        <button className="button">Send</button>
+      </div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {messagesReceived.map((message) => (
+          <div>{message}</div>
+        ))}
+        {messagesSent.map((message) => (
+          <div>{message}</div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
