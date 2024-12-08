637d54a7a383fa3f677b2075b4cc9d71cefecedb
Emily Kemp <emilyandalicekemp@gmail.com>
2024-12-08 17:12:54 +1300

    changed cors origin to render url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://message-app-jbm8.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
