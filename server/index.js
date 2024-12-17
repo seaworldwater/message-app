2fc7a91d841f040346e1cd0960995505a56e4859
Emily Kemp <emilyandalicekemp@gmail.com>
2024-12-18 10:31:42 +1300

    added res to possible get request

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ const io = new Server(server, {
   },
 });
 
+app.get("/", (req, res) => {
+  res.send("WebSocket server is running!");
+});
+
 function calculatePeopleInRoom(socket, room, minus) {
   let size = io.sockets.adapter.rooms.get(room).size;
   let info = [room, size - minus];
